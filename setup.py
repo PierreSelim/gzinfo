46be344452243c475133b1ff25373e3fd06d831e
Pierre-Selim <ps.huard@gmail.com>
2020-04-19 14:24:37 +0200

    1.0.1: Adding description to PyPi project

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
+import os
 from setuptools import setup, find_packages
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 packages = sorted(find_packages())
@@ -17,6 +18,11 @@ classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8"]
 
+# description
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='gzinfo',
       version=__version__,
@@ -24,6 +30,8 @@ setup(name='gzinfo',
       author_email='pierre-selim@huard.info',
       url='https://github.com/PierreSelim/gzinfo',
       description='Retrieving archive filename from gz files in Python ',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       license='MIT Licence',
       classifiers=classifiers,
       packages=packages,
